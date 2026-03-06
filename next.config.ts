eba9d54a239c0a0a748eae7041648439d28cfa1f
Douglas Kavita <douglaskavita@gmail.com>
2026-03-07 01:31:52 +0300

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivity: false,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
